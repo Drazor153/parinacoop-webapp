b84b05e2744a0446501701a569b921c9caa4e36e
Drazor153 <fabianjusto2341@gmail.com>
2024-09-23 14:09:23 -0300

    feat(project structure): add auth pages

---
+++ src/app/core/auth/auth.routes.ts
@@ -0,0 +1,18 @@
+import { Route } from "@angular/router";
+
+const routes: Route[] = [
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component')
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.component')
+    },
+    {
+        path: 'password-recovery',
+        loadComponent: () => import('./pages/password-recovery/password-recovery.component')
+    }
+]
+
+export default routes;
