be0b44cfd2b84a7b84a3021c3f94f8e7e7b8902e
Drazor153 <fabianjusto2341@gmail.com>
2024-09-28 02:10:17 -0300

    refactor: project structure setted

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,18 +1,26 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 
 const routes: Route[] = [
-    {
+  {
+    path: '',
+    loadComponent: () =>
+      import('@core/auth-layout/auth-layout.component'),
+    children: [
+      {
         path: 'login',
-        loadComponent: () => import('./pages/login/login.component')
-    },
-    {
+        loadComponent: () => import('./pages/login/login.component'),
+      },
+      {
         path: 'register',
-        loadComponent: () => import('./pages/register/register.component')
-    },
-    {
+        loadComponent: () => import('./pages/register/register.component'),
+      },
+      {
         path: 'password-recovery',
-        loadComponent: () => import('./pages/password-recovery/password-recovery.component')
-    }
-]
+        loadComponent: () =>
+          import('./pages/password-recovery/password-recovery.component'),
+      },
+    ],
+  },
+];
 
 export default routes;
--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export default class LoginComponent {
-
+  clientLoginForm = new FormGroup({
+    rut: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  
+  constructor() {}
+  
+  onSubmit() {
+    console.log(this.clientLoginForm.value);
+  }
 }
