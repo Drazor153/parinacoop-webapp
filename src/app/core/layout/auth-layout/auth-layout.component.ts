f996a842c568c4d678506586c6506fae95dbfb2f
Drazor153 <fabianjusto2341@gmail.com>
2024-11-06 15:44:46 -0300

    refactor: header and footer componetized

--- src/app/core/layout/auth-layout/auth-layout.component.ts
+++ src/app/core/layout/auth-layout/auth-layout.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
-  styleUrl: './auth-layout.component.scss'
+  styleUrl: './auth-layout.component.scss',
 })
-export default class AuthLayoutComponent {
-
-}
+export default class AuthLayoutComponent {}
