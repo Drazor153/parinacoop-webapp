8901c8eb98bce6e3e5525ef4132c17d11fb736ad
Drazor153 <fabianjusto2341@gmail.com>
2024-11-06 16:21:42 -0300

    guard implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadChildren: () => import('./core/auth/auth.routes')
-    }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layout/auth-layout/auth-layout.component'),
+    canActivate: [authGuard],
+    loadChildren: () => import('./core/auth/auth.routes'),
+  },
 ];
--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,30 +1,24 @@
 import { Route } from '@angular/router';
+import { authGuard } from './auth.guard';
 
 const routes: Route[] = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component'),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.component'),
+  },
+  {
+    path: 'password-recovery',
     loadComponent: () =>
-      import('@core/layout/auth-layout/auth-layout.component'),
-    children: [
-      {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full',
-      },
-      {
-        path: 'login',
-        loadComponent: () => import('./pages/login/login.component'),
-      },
-      {
-        path: 'register',
-        loadComponent: () => import('./pages/register/register.component'),
-      },
-      {
-        path: 'password-recovery',
-        loadComponent: () =>
-          import('./pages/password-recovery/password-recovery.component'),
-      },
-    ],
+      import('./pages/password-recovery/password-recovery.component'),
   },
 ];
 
